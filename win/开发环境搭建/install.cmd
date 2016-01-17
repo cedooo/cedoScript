8a549acc59fb2f4bcbedca04ec23cc22da662fbe
cedo <cedoo@qq.com>
2016-01-18 00:16:13 +0800

    error fix

--- win/开发环境搭建/install.cmd
+++ win/开发环境搭建/install.cmd
@@ -9,7 +9,7 @@ del %systemdrive%\7zip.msi
 
 set zipdrive=
 set /p "zipdrive=请输入ZIP所在路径（或回车默认路径为%systemdrive%\devenv.zip:"
-if defined input (echo ZIP所在路径已设置) else (set input=%systemdrive%\devenv.zip)
+if defined input (echo ZIP所在路径已设置) else (set zipdrive=%systemdrive%\devenv.zip)
 
 ::解压devenv文件到C盘根目录
 "%programfiles%"\7-zip\7z e -y -spf %systemdrive%\devenv.zip -o%systemdrive%\
