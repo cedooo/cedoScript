d2a1b131ba8f926ff04c1707c104607e21e9a06a
CeDo <cedoo@qq.com>
2016-02-02 12:26:02 +0800

    Update run.cmd

--- win/vpnproxy/run.cmd
+++ win/vpnproxy/run.cmd
@@ -1,5 +1,5 @@
 @echo off
 cd /d %~dp0
 ::relink
-plink.exe -N -pw 3846721 vpntech@s2.avpn.us -P 2224 -D 127.0.0.1:1081
+plink.exe -N -pw ****** vpntech@s2.avpn.us -P 2224 -D 127.0.0.1:1081
 ::goto :relink
