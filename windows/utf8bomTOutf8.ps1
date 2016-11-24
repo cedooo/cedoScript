54509d25abdc12c99a721b97e86e2078a01ece70
cedoo <cedoo@qq.com>
2016-11-24 21:58:36 +0800

    :sparkles: add [ utf-8-bom covert to utf-8] powershell script

---
+++ windows/utf8bomTOutf8.ps1
@@ -0,0 +1,6 @@
+$list = Get-ChildItem .\ -recurse *.java|%{$_.FullName}
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
+foreach ($i in $list){
+    $a = Get-Content $i
+    [System.IO.File]::WriteAllLines($i, $a, $Utf8NoBomEncoding)
+}
